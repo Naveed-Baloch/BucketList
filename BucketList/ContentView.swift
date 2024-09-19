ce7b453dc7c6abe3cd969efdc04454a6727895c7
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-19 20:32:54 +0500

    add operator overloading

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,13 +7,41 @@
 
 import SwiftUI
 
+struct User : Identifiable, Comparable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.firstName < rhs.firstName
+    }
+}
+
+
 struct ContentView: View {
+    
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            List(users) { user in
+                Text("\(user.lastName)")
+            }
+            Button("Compare 1st Two Users") {
+                let firstUser = users[0]
+                let secondUser = users[1]
+               
+                print(firstUser > secondUser)
+            }
         }
         .padding()
     }
