e45436ef46f004cccc68d898b6994b848e84ebfe
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-19 18:49:04 +0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Naveed on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
