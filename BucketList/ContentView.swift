1b73495bd8c58635b3d5d29ca21ac3a1915f230d
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-19 21:39:39 +0500

    Add Basic Google Map Setup

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,58 +6,13 @@
 //
 
 import SwiftUI
-
-struct User : Identifiable, Comparable {
-    let id = UUID()
-    var firstName: String
-    var lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.firstName < rhs.firstName
-    }
-}
+import MapKit
 
 
 struct ContentView: View {
-    
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
-    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            List(users) { user in
-                Text("\(user.lastName)")
-            }
-            Button("Compare 1st Two Users") {
-                let firstUser = users[0]
-                let secondUser = users[1]
-               
-                print(firstUser > secondUser)
-            }
-            
-            Button("Write & Read File") {
-                let documentDirectoryPath = URL.documentsDirectory
-                let filePath = documentDirectoryPath.appending(path: "message.txt")
-                let data = Data("Hello from the File Contnet".utf8)
-                
-                do {
-                    try data.write(to: filePath, options: [.atomic, .completeFileProtection])
-                    let input = try String(contentsOf: filePath)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        .padding()
+        Map()
+            .mapStyle(.imagery)
     }
 }
 
