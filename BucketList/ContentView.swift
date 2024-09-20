e4fad3203bd6612255a15fa9699b9563d8f7ac22
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-20 21:05:16 +0500

    add camera position using two way bindings

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -17,10 +17,41 @@ struct ContentView: View {
         )
     )
     
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
     var body: some View {
-        Map(initialPosition: initialPostion,
-            interactionModes: [.zoom, .rotate, .pan])
+        VStack {
+            Map(
+                position: $position,
+                interactionModes: [.zoom, .rotate, .pan]
+            )
             .mapStyle(.imagery)
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
     }
 }
 
