5656280cddb3e7b1d5d4e4ef7e6964b0e0c0b245
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-20 21:03:04 +0500

    add initial position

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -10,8 +10,16 @@ import MapKit
 
 
 struct ContentView: View {
+    let initialPostion = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
     var body: some View {
-        Map()
+        Map(initialPosition: initialPostion,
+            interactionModes: [.zoom, .rotate, .pan])
             .mapStyle(.imagery)
     }
 }
